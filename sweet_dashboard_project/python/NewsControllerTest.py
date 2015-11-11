38944b016d3165085b969618f52fc3d35063e527
gazala <ghazala1606@gmail.com>
2015-11-11 10:58:16 +0530

    newswhip added

---
+++ sweet_dashboard_project/python/NewsControllerTest.py
@@ -0,0 +1,39 @@
+import pprint
+import csv
+import json
+
+from NewsWhipAPILib.Controllers.NewsController import *
+
+controller = NewsController()
+"""
+response = controller.get_region(key='AHwaqz7hApx9D')
+#print response["articles"][0]["fb_data"]["like_count"]
+articles = response["articles"]
+for article in articles:
+	print article["fb_data"]["comment_count"] , "," , article["fb_data"]["like_count"]
+	#print article["fb_data"]["like_count"]
+#print json.dumps(response)
+
+#pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(response)
+
+"""
+
+
+
+response = controller.get_region(key='AHwaqz7hApx9D') 
+artriclesArr = response["articles"]
+with open('news2.csv', 'w') as csvfile:
+	writer=csv.writer(csvfile, delimiter=',',lineterminator='\n',)
+    #data = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+   
+    #data.writerow(['headline', 'comment_count', 'share_count', 'tw_count'])
+	for article in artriclesArr:
+		headlineStr = article["headline"]
+		headlineStr.encode("utf-8")
+		#headline = unicode(headlineStr, "utf-8")
+		writer.writerow([headlineStr, article["fb_data"]["comment_count"], article["fb_data"]["like_count"] ])
+
+f.close()
+
+
