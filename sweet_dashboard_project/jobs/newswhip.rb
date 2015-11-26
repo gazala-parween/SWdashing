a55f742d497b83398d65d7a2c7f1719a4bc3d334
gazala <ghazala1606@gmail.com>
2015-11-26 13:32:42 +0530

    complete youtube_player

--- sweet_dashboard_project/jobs/newswhip.rb
+++ sweet_dashboard_project/jobs/newswhip.rb
@@ -1,43 +1,38 @@
-require 'net/http'
+# This job displays all the newswhip articles of region india from last 2hrs 5 at a time by accessing the public available api of newswhip.
 
+require 'net/http'
 require 'json'
 
 min = 0
 page_size= 5
-#max_length = min + page_size
-SCHEDULER.every '50s', :first_in => 0 do |job|
-  uri = URI('http://www.newswhip.com/api/v1/region/India/all/6?key=AHwaqz7hApx9D')
-  response = Net::HTTP.get(uri) # => String
 
+# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+
+SCHEDULER.every '2m', :first_in => 0 do |job|
+  uri = URI('http://www.newswhip.com/api/v1/region/India/all/2?key=AHwaqz7hApx9D')
+  response = Net::HTTP.get(uri) # => String
 
   data = JSON.parse(response)
 
   newswhip_article = Array.new
 
-  #newswhip_ar= []
-  #i=0
   data['articles'].each do |arr|
-      newswhip_article.push({
-      label: arr['headline'],
-	  value: arr['source']['publisher'],
-	  #value2: arr['fb_data']['like_count'],
-      value2: arr['fb_data']['like_count'] + arr['tw_data']['tw_count']})
-	#if i<20
-		#newswhip_ar << {:headline=>arr['headline'],:fb_like=>arr['fb_data']['like_count'],:tw_count=>arr['tw_data']['tw_count']}  
-	#end
-	#i+=1
-	end
-	max = newswhip_article.length
-	#puts newswhip_ar
-# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-     while max > (min+page_size)
-        send_event('news', { items: newswhip_article.slice(min, page_size)})
-	   #puts min
-		sleep(10)
+    newswhip_article.push({
+    label: arr['headline'],
+	value: arr['source']['publisher'],
+	value2: arr['fb_data']['like_count'] + arr['tw_data']['tw_count']})
+  end
+	
+  max = newswhip_article.length
+	
+  while max > (min+page_size)
+    send_event('news', { items: newswhip_article.slice(min, page_size)})
+	sleep(10)
 	 
-	    min = min + page_size
-        #max = max + page_size
-	 end
-	 send_event('news', { items: newswhip_article.slice(min..max)})
+	min = min + page_size
+        
+  end
+  
+  send_event('news', { items: newswhip_article.slice(min..max)})
 	 
 end
--- sweet_dashboard_project/jobs/youtube_play.rb
+++ sweet_dashboard_project/jobs/youtube_play.rb
@@ -1,101 +1,53 @@
-#url = www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCx2HcmpB-UZGkMXOCJ4QIVA&maxResults=5&key=AIzaSyBlRhNVLNqIO9UBBfw8HtV2MZkMeS0Y_q0
-
 require 'net/http'
 require 'openssl'
 require 'json'
 
-# This job can track some metrics of a single youtube video by accessing the
-# public available api of youtube.
-
-# Config
-# ------
-youtube_api_key =  ENV['YOUTUBE_API_KEY'] || 'AIzaSyBlRhNVLNqIO9UBBfw8HtV2MZkMeS0Y_q0'
-youtube_channel_id = ENV['YOUTUBE_CHANNEL_ID'] || 'UCx2HcmpB-UZGkMXOCJ4QIVA' #'UCNJcSUSzUeFm8W9P7UUlSeQ'
-max_results = 5
-
-
-#for each in youtube_channel_id do
-
 SCHEDULER.every '5m', :first_in => 0 do |job|
   
-  http = Net::HTTP.new("www.googleapis.com", Net::HTTP.https_default_port())
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE # disable ssl certificate check
-  response = http.request(Net::HTTP::Get.new("/youtube/v3/search?part=snippet&channelId=#{youtube_channel_id}&order=date&maxResults=#{max_results}&key=#{youtube_api_key}"))
+  def video_stats(v_id)
+	uri = URI("https://www.googleapis.com/youtube/v3/videos?part=statistics&id=
+	"+v_id+"&key=AIzaSyBlRhNVLNqIO9UBBfw8HtV2MZkMeS0Y_q0")
+    response = Net::HTTP.get(uri) # => String
+    data = JSON.parse(response)
+	return data['items'][0]['statistics']['viewCount']
+  end
+
+  uri=URI('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCx2HcmpB-UZGkMXOCJ4QIVA&order=date&maxResults=5&key=AIzaSyBlRhNVLNqIO9UBBfw8HtV2MZkMeS0Y_q0')
+  response = Net::HTTP.get(uri) # => String
   
-  
-  if response.code != "200"
-    puts "youtube api error (status-code: #{response.code})\n#{response.body}"
-  else
-    data = JSON.parse(response.body, :symbolize_names => true)
+  data = JSON.parse(response)
 	
-	youtube_videos = Array.new
+  youtube_videos = Array.new
 	
-    data[:items].each do |video|
+  data['items'].each do |video|
       youtube_videos.push({
-        label: video[:snippet][:title],
-        value: video[:id][:videoId]
+        label: video['snippet']['title'],
+        value: video['id']['videoId'],
+		value1:video_stats(video['id']['videoId'])
       })
-    end
+   end   
    
-   
-   #video_ar = []
-   # puts data[:items]
-   
-   # for item in data[:items]
-	# puts item,"  " 
-   # end
-   
-   #puts youtube_videos
-   
-   
-   
-    youtube_stats = Array.new
-    for each in youtube_videos do
-      http = Net::HTTP.new("www.googleapis.com", Net::HTTP.https_default_port())
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE # disable ssl certificate check
-      response = http.request(Net::HTTP::Get.new("/youtube/v3/videos?part=statistics&id=#{each[:value]}&key=#{youtube_api_key}"))
-      data = JSON.parse(response.body, :symbolize_names => true)
-
-      data[:items].each do |video|
-        youtube_stats.push({
-          label: each[:label],
-		  value: each[:value],
-          value1: video[:statistics][:viewCount].to_i
-        })
-      end
-    end
-	#def video_detil
-	
-   
- 
- 
-   url_array = Array.new
-   
-   for each in youtube_stats do
-	   
-     url = "http://www.youtube.com/embed/#{each[:value]}?autoplay=1"
-	  	
-	   url_array.push({
-          :yurl => url,
-		  :title => each[:label],
-		  :count => each[:value1]
-        })
-       end
-     puts url_array
-     
-	 min=0
-     len=1
- 
-     for each in url_array
-	
-          send_event('youtube_player', { :items => url_array.slice(min, len) })
+  #puts youtube_videos
   
-          sleep(60)
-          min=min+1
+  url_array = Array.new
+   
+  for each in youtube_videos do
+	url = "http://www.youtube.com/embed/#{each[:value]}?autoplay=1"
+	  
+	url_array.push({
+        :yurl => url,
+		:title => each[:label],
+		:count => each[:value1] 
+        } )
+  end
   
-     end
-
-	end
+  #puts url_array
+  
+  min=0
+  len=1  
+  for each in url_array
+	send_event('youtube_player', { :items => url_array.slice(min, len) })
+    sleep(60)
+    min=min+1    
+  end
 end
